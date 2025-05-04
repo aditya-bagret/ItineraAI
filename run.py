8a427314736f2f5fd95c2a7e61f1b55dc0e26079
Aditya Sharma <adityasharma4work@gmail.com>
2025-05-04 18:01:13 +0530

    css change

--- run.py
+++ run.py
@@ -10,3 +10,4 @@ if __name__ == '__main__':
     
     # Run the application
     app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)), debug=os.getenv('DEBUG', 'True').lower() == 'true')
+    
